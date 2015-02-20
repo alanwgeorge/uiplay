6061e7ae1574922f0c1bc1d1f0e2ca2c253d5ad5
alan.wesley.george@gmail.com <alan.wesley.george@gmail.com@fb3cdd95-ef15-6831-849a-d5ddf551276d>
2015-02-20 02:07:15 +0000

    redid scrolling

--- trunk/app/src/main/java/com/example/android/uiplay/AccountListFragment.java
+++ trunk/app/src/main/java/com/example/android/uiplay/AccountListFragment.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import com.example.android.uiplay.model.Account;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -59,12 +60,14 @@ public class AccountListFragment extends ListFragment {
         accountArrayAdapter = new AccountArrayAdapter(getActivity(), 0, new ArrayList<Account>());
         setListAdapter(accountArrayAdapter);
         accountArrayAdapter.setNotifyOnChange(true);
+        accountArrayAdapter.add(new Account("My Account", 100000, "Available Balance", "...5463"));
 
         return rootView;
     }
 
     private void addAccountRow() {
-        accountArrayAdapter.add(new Account("My Account", 100000, "Available Balance", "...5463"));
+        Random random = new Random();
+        accountArrayAdapter.add(new Account("My Account" + getListAdapter().getCount(), random.nextInt(100000000), "Available Balance", "...5463"));
     }
 
 }
