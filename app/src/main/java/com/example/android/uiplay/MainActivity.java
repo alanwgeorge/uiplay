61c764654f0070e3ef655a1499d41e371d9a7dbf
alan.wesley.george@gmail.com <alan.wesley.george@gmail.com@fb3cdd95-ef15-6831-849a-d5ddf551276d>
2015-02-21 20:06:03 +0000

    added navigation drawer

--- app/src/main/java/com/example/android/uiplay/MainActivity.java
+++ app/src/main/java/com/example/android/uiplay/MainActivity.java
@@ -1,21 +1,114 @@
 package com.example.android.uiplay;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements NavigationDrawerFragment.NavigationDrawerCallbacks {
     private static final String TAG = "MainActivity";
+    public static final String ARG_SECTION_NUMBER = "section_number";
+
+    private NavigationDrawerFragment navigationDrawerFragment;
+    private CharSequence title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d(TAG, "onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new AccountListFragment())
-                    .commit();
+//        if (savedInstanceState == null) {
+//            getFragmentManager().beginTransaction()
+//                    .add(R.id.container, new AccountListFragment())
+//                    .commit();
+//        }
+
+        navigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager().findFragmentById(R.id.navigation_drawer);
+        title = getTitle();
+
+        // Set up the drawer.
+        navigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
+    }
+
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        FragmentManager fragmentManager = getFragmentManager();
+        Fragment fragment;
+
+        switch (position) {
+            case 0:
+                fragment = new AccountListFragment();
+                break;
+            default:
+                fragment = new PlaceholderFragment();
+        }
+
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, position + 1);
+        fragment.setArguments(args);
+
+        fragmentManager.beginTransaction().replace(R.id.container, fragment).commit();
+    }
+
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                title = getString(R.string.title_section1);
+                break;
+            case 2:
+                title = getString(R.string.title_section2);
+                break;
+            case 3:
+                title = getString(R.string.title_section3);
+                break;
         }
     }
+
+    public void restoreActionBar() {
+        ActionBar actionBar = getActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(title);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!navigationDrawerFragment.isDrawerOpen()) {
+            getMenuInflater().inflate(R.menu.global, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() { }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_placeholder, container, false);
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+
 }
